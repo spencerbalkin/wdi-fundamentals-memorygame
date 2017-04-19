5af880889dff5e60d52bfac301b27cd9675cf8c0
Spencer Balkin <SpencerBalkin@Freds-MacBook-Pro.local>
2017-04-18 21:11:36 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
- console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay === 2) {
+	result = 'true';
+}	else if (cardsInPlay[0] === cardsInPlay[1]) {
+	result = alert("You found a match!");
+}	else {
+	result = alert("Sorry, try again.")
+}
