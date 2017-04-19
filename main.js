89a6373b31921af401980d7c0bc4c69543a477c5
Spencer Balkin <SpencerBalkin@Freds-MBP.home>
2017-04-19 16:12:38 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var checkForMatch = function () {
+	  if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+};
 
-if (cardsInPlay === 2) {
+var flipCard = function (cardId) {
+checkForMatch ();
+//if I remove ^^ then it says "flipped queen" and king without the "sorry, try again"
+	if (cardsInPlay === 2) {
 	result = 'true';
 }	else if (cardsInPlay[0] === cardsInPlay[1]) {
 	result = alert("You found a match!");
 }	else {
 	result = alert("Sorry, try again.")
 }
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
