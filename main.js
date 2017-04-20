bdd3536c4c44a7b871947db7810185c57f40f195
Spencer Balkin <SpencerBalkin@Freds-MBP.home>
2017-04-20 02:57:11 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",	
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",	
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+},
+];
+
 var cardsInPlay = [];
 
-var checkForMatch = function () {
-	  if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function() {
+	   if (cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
   } else {
       console.log("Sorry, try again.");
   }
 };
 
+
 var flipCard = function (cardId) {
-checkForMatch ();
-//if I remove ^^ then it says "flipped queen" and king without the "sorry, try again"
+  checkForMatch ();
+//if I remove ^^ then it says "flipped queen" and king without the "you found a match"
 	if (cardsInPlay === 2) {
 	result = 'true';
 }	else if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -19,8 +42,10 @@ checkForMatch ();
 }	else {
 	result = alert("Sorry, try again.")
 }
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].suit)
+    console.log(cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].rank);
 };
 
 flipCard(0);
